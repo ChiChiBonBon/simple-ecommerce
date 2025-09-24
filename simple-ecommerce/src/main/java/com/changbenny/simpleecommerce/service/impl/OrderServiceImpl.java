40d2199b386d2a34bce0cb3e7fc044ecc92b688c
LAPTOP-18PL9HOH\USER <a0918235320@gmail.com>
2025-09-24 14:14:25 +0800

    修改:統一log的處理

--- simple-ecommerce/src/main/java/com/changbenny/simpleecommerce/service/impl/OrderServiceImpl.java
+++ simple-ecommerce/src/main/java/com/changbenny/simpleecommerce/service/impl/OrderServiceImpl.java
@@ -11,7 +11,8 @@ import com.changbenny.simpleecommerce.repository.OrderRepository;
 import com.changbenny.simpleecommerce.service.OrderService;
 import com.changbenny.simpleecommerce.service.ProductService;
 import com.changbenny.simpleecommerce.service.UserService;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -22,9 +23,10 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-@Slf4j
+
 @Service
 public class OrderServiceImpl implements OrderService {
+    private static final Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Autowired
     private OrderRepository orderRepository;
