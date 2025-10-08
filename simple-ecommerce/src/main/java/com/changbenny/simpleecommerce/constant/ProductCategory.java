6dbafe4f8a6e92b3ca77c6a12b76451aacad0bcd
LAPTOP-18PL9HOH\USER <a0918235320@gmail.com>
2025-10-08 10:54:44 +0800

    程式修改清單: ProductCategory.java 修改enum

--- simple-ecommerce/src/main/java/com/changbenny/simpleecommerce/constant/ProductCategory.java
+++ simple-ecommerce/src/main/java/com/changbenny/simpleecommerce/constant/ProductCategory.java
@@ -1,8 +1,8 @@
 package com.changbenny.simpleecommerce.constant;
 
-//建立enum 商品分類
+// 建立 enum 商品分類
 public enum ProductCategory {
-    FOOD,
-    CAR,
-    E_BOOK
+    FRESH_PRODUCE,   // 新鮮食材（蔬果、米、雜糧）
+    HEALTH_FOOD,     // 健康食品/保健品
+    SNACKS_DRINKS    // 加工食品、零食、飲品
 }
